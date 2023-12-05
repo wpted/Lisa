491804d25a471212b47f9b65a359a56c0f791946
wpted <silverhandsome@gmail.com>
2023-12-05 08:00:19 +0800

    Feat: Add token package, type TokenType and Token.

---
+++ token/token.go
@@ -0,0 +1,34 @@
+package token
+
+const (
+	// ILLEGAL signifies a token/character we don't know illegal.
+	ILLEGAL = "ILLEGAL"
+	// EOF means 'End Of File', which tells our parser later on that it can stop.
+	EOF = "EOF"
+
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	ASSIGN = "="
+	PLUS   = "+"
+
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	FUNCTION = "FUNCTION"
+	VAR      = "VAR"
+)
+
+type TokenType string
+
+// Token is the transformation result of lexing source code.
+type Token struct {
+	Type TokenType
+	// Literal is the parsed value of a token.
+	Literal string
+}

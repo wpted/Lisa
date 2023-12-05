5e6964807a32ff1915a960726bf6d10b33bb718d
wpted <silverhandsome@gmail.com>
2023-12-05 16:02:26 +0800

    Test: Create tests for function and methods in lexer.go.

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -5,17 +5,154 @@ import (
 	"testing"
 )
 
+func TestNew(t *testing.T) {
+	testInput := "test"
+	l := New("test")
+	if l.input != testInput {
+		t.Errorf("error initializing lexer - wrong input: expected %s, have %s.\n", testInput, l.input)
+	}
+	if l.position != 0 {
+		t.Errorf("error initializing lexer - wrong position: expected %d, have %d.\n", 0, l.position)
+	}
+	if l.readPosition != 1 {
+		t.Errorf("error initializing lexer - wrong read position: expected %d, have %d.\n", 1, l.readPosition)
+	}
+	if l.ch != testInput[0] {
+		t.Errorf("error initializing lexer - wrong character: expected %q, have %q.\n", testInput[0], l.ch)
+	}
+}
+
+func TestLexer_readChar(t *testing.T) {
+	testCases := []struct {
+		input            string
+		jumps            int
+		expectedPosition int
+		expectedCurrChar byte
+	}{
+		{"test 32", 3, 3, 't'},
+		{"32", 3, 3, 0},
+	}
+	var lex *Lexer
+	for i, tc := range testCases {
+		lex = New(tc.input)
+		for i := 0; i < tc.jumps; i++ {
+			lex.readChar()
+		}
+		if tc.expectedPosition != lex.position {
+			t.Errorf("tests[%d] - error reading char [wrong position]: expected %d, got %d.\n", i, tc.expectedPosition, lex.position)
+		}
+		if tc.expectedCurrChar != lex.ch {
+			t.Errorf("tests[%d] - error reading char [wrong character]: expected %q, got %q.\n", i, tc.expectedCurrChar, lex.ch)
+		}
+	}
+}
+
+func TestLexer_readIdentifier(t *testing.T) {
+	testCases := []struct {
+		input         string
+		expectedIdent string
+	}{
+		{"test 32", "test"},
+		{"32", ""},
+	}
+	var lex *Lexer
+	for i, tc := range testCases {
+		lex = New(tc.input)
+		re := lex.readIdentifier()
+		if re != tc.expectedIdent {
+			t.Errorf("tests[%d] - error reading number: expected %s, got %s.\n", i, tc.expectedIdent, re)
+		}
+	}
+}
+
+func Test_isLetter(t *testing.T) {
+	testCases := []struct {
+		ch       byte
+		isLetter bool
+	}{
+		{'0', false},
+		{'a', true},
+		{' ', false},
+		{'A', true},
+		{'_', true},
+	}
+
+	for i, tc := range testCases {
+		re := isLetter(tc.ch)
+		if re != tc.isLetter {
+			t.Errorf("tests[%d] - error isDigit: expected %v, got %v.\n", i, tc.isLetter, re)
+		}
+	}
+}
+
+func TestLexer_readNumber(t *testing.T) {
+	testCases := []struct {
+		input          string
+		expectedNumber string
+	}{
+		{"32 fds", "32"},
+		{"32", "32"},
+	}
+	var lex *Lexer
+	for i, tc := range testCases {
+		lex = New(tc.input)
+		re := lex.readNumber()
+		if re != tc.expectedNumber {
+			t.Errorf("tests[%d] - error reading number: expected %s, got %s.\n", i, tc.expectedNumber, re)
+		}
+	}
+}
+
+func Test_isDigit(t *testing.T) {
+	testCases := []struct {
+		ch      byte
+		isDigit bool
+	}{
+		{'0', true},
+		{'a', false},
+		{' ', false},
+	}
+
+	for i, tc := range testCases {
+		re := isDigit(tc.ch)
+		if re != tc.isDigit {
+			t.Errorf("tests[%d] - error isDigit: expected %v, got %v.\n", i, tc.isDigit, re)
+		}
+	}
+}
+
+func TestLexer_skipWhiteSpace(t *testing.T) {
+	testCases := []struct {
+		input            string
+		expectedPosition int
+	}{
+		{" fds", 1},
+		{"   fds", 3},
+		{"\nfds", 1},
+		{"\tfds", 1},
+		{"\rfds", 1},
+	}
+	var lex *Lexer
+	for i, tc := range testCases {
+		lex = New(tc.input)
+		lex.skipWhiteSpace()
+		if lex.position != tc.expectedPosition {
+			t.Errorf("tests[%d] - error reading number: expected %d, got %d.\n", i, tc.expectedPosition, lex.position)
+		}
+	}
+}
+
 func TestLexer_NextToken(t *testing.T) {
 	tokenTests := []struct {
-		input        string
-		parsedResult []struct {
+		input                 string
+		expectedParsedResults []struct {
 			expectedType    token.LexicalType
 			expectedLiteral string
 		}
 	}{
 		{
 			input: `=+(){},;`,
-			parsedResult: []struct {
+			expectedParsedResults: []struct {
 				expectedType    token.LexicalType
 				expectedLiteral string
 			}{
@@ -36,9 +173,8 @@ func TestLexer_NextToken(t *testing.T) {
 					var add = fn(x, y) {
      					return x + y;
 					};
-   					var result = add(five, ten);
-   					`,
-			parsedResult: []struct {
+   					var result = add(five, ten);`,
+			expectedParsedResults: []struct {
 				expectedType    token.LexicalType
 				expectedLiteral string
 			}{
@@ -92,15 +228,15 @@ func TestLexer_NextToken(t *testing.T) {
 	l := new(Lexer)
 	for i, tt := range tokenTests {
 		l = New(tt.input)
-		for tokenIdx, tc := range tt.parsedResult {
+		for tokenIdx, pr := range tt.expectedParsedResults {
 			tok := l.NextToken()
-			if tok.Type != tc.expectedType {
+			if tok.Type != pr.expectedType {
 				// %q is a single-quoted character literal safely escaped with Go syntax.
-				t.Errorf("tests[%d], token[%d] - error token type: expected %q, got %q.\n", i, tokenIdx, tc.expectedType, tok.Type)
+				t.Errorf("tests[%d], token[%d] - error token type: expected %q, got %q.\n", i, tokenIdx, pr.expectedType, tok.Type)
 			}
 
-			if tok.Literal != tc.expectedLiteral {
-				t.Errorf("tests[%d], token[%d] - error token literal value: expected %q, got %q.\n", i, tokenIdx, tc.expectedLiteral, tok.Literal)
+			if tok.Literal != pr.expectedLiteral {
+				t.Errorf("tests[%d], token[%d] - error token literal value: expected %q, got %q.\n", i, tokenIdx, pr.expectedLiteral, tok.Literal)
 			}
 		}
 	}

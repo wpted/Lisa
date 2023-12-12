54b2006054a6a404d8faaa09bcbd1403ce89d4ad
wpted <silverhandsome@gmail.com>
2023-12-12 12:02:21 +0800

    Feat: Create logic for parsing a 'VarStatement'. Store the error occur by adding field errors in 'Parser'.

--- parser/parser.go
+++ parser/parser.go
@@ -4,13 +4,15 @@ import (
 	"Lisa/ast"
 	token "Lisa/lexToken"
 	"Lisa/lexer"
+	"fmt"
 )
 
 // Parser is a component that takes the input data, and builds a data structure, checking for correct syntax in the process.
 // Out parser utilizes a *lexer.Lexer and is responsible for building an *ast.ProgramRoot (which is a tree) from the input.
 type Parser struct {
 	// l is a pointer to an instance of the lexer.
-	l *lexer.Lexer
+	l      *lexer.Lexer
+	errors []string
 
 	// curToken points to the current token.
 	curToken *token.Token
@@ -24,6 +26,7 @@ type Parser struct {
 func New(l *lexer.Lexer) *Parser {
 	p := &Parser{
 		l:         l,
+		errors:    make([]string, 0),
 		curToken:  nil,
 		nextToken: nil,
 	}
@@ -38,6 +41,11 @@ func New(l *lexer.Lexer) *Parser {
 	p.readNextToken()
 
 	return p
+}
+
+// Errors returns all the errors occur when parsing an input with a Parser.
+func (p *Parser) Errors() []string {
+	return p.errors
 }
 
 // readNextToken helps advances the token in the lexer by 1, simultaneously setting the curToken and nextToken.
@@ -56,9 +64,10 @@ func (p *Parser) ParseProgram() *ast.ProgramRoot {
 	}
 
 	// Determine when to stop the recursive parsing -> When current token type is token.EOF.
-	for p.curToken.Type != token.EOF {
+	for !p.curTokenTypeIs(token.EOF) {
 		// Parse the statement one by one from the input and append it to astRoot.
 		stmt := p.parseStatement()
+		// Nil stmt means that there's something wrong in the given input.
 		if stmt != nil {
 			astRoot.Statements = append(astRoot.Statements, stmt)
 		}
@@ -68,6 +77,101 @@ func (p *Parser) ParseProgram() *ast.ProgramRoot {
 	return astRoot
 }
 
+// parseStatement is a helper function that checks the type of the curToken and determine what statement to return to ParseProgram.
+// parseStatement stops when reaches a ';' (type token.SEMICOLON).
 func (p *Parser) parseStatement() ast.Statement {
-	return nil
+
+	// Check what statement we're currently parsing.
+	switch p.curToken.Type {
+	case token.VAR:
+		return p.parseVarStatement()
+	//case token.RETURN:
+	default:
+		return nil
+	}
 }
+
+// parseVarStatement parses a statement that starts with 'var' and ends with ';'.
+// If there's any elements missing from a standard 'var x = 5;' statement, the parser stores all the error in errors and returns a nil ast.VarStatement.
+func (p *Parser) parseVarStatement() *ast.VarStatement {
+	invalidStmt := false
+
+	// When parsing 'var x = 5;'
+
+	// 1. Assign 'var' token to the 'Token' field for the VarStatement.
+	stmt := &ast.VarStatement{Token: p.curToken}
+
+	// 2. Peek the next token, which we expect it is the variable name 'x', a type token.IDENT.
+	// If the token is an expected type, advance the token pointer.
+	if !p.expectNext(token.IDENT) {
+		// Not an expected Identifier, store the error.
+		p.storeNextTokenTypeError(token.IDENT)
+		invalidStmt = true
+		// Since we're missing the identifier, we expect the next token type to be a type token.ASSIGN.
+	}
+
+	// Set the name of stmt to the parsed variable name.
+	stmt.Name = &ast.IdentifierExpression{
+		// Since we already advanced the token when checking whether the token is a type token.IDENT,
+		// p.curToken is now the name of the variable. ('x' in 'var x = 5;')
+		Token: p.curToken,
+		Value: p.curToken.Literal,
+	}
+
+	// 3. Peek the next token, which we expect it is a '=', a type token.ASSIGN.
+	// If the token is an expected type, advance the token pointer.
+	if !p.expectNext(token.ASSIGN) {
+		p.storeNextTokenTypeError(token.ASSIGN)
+		invalidStmt = true
+	}
+
+	// 4. Check and assign the expression to stmt.Value.
+	p.readNextToken()
+
+	// 5. Check the semicolon at the end of a var statement.
+	if !p.expectNext(token.SEMICOLON) {
+		// Not an expected Identifier, store the error.
+		p.storeNextTokenTypeError(token.SEMICOLON)
+		invalidStmt = true
+	}
+
+	// After reached a supposed ';' position return the parsed statement, check whether the statement is valid.
+	if invalidStmt {
+		return nil
+	}
+	return stmt
+}
+
+// curTokenTypeIs checks whether the type of the current token is identical as the given type.
+func (p *Parser) curTokenTypeIs(expectType token.LexicalType) bool {
+	return p.curToken.Type == expectType
+}
+
+// nextTokenTypeIs checks whether the type of the next token is identical as the given type.
+func (p *Parser) nextTokenTypeIs(expectType token.LexicalType) bool {
+	return p.nextToken.Type == expectType
+}
+
+// expectNext checks the next token type, and if the nextToken is identical as the given type, advance the pointer and return true.
+func (p *Parser) expectNext(expectType token.LexicalType) bool {
+	if p.nextTokenTypeIs(expectType) {
+		// If the token is the expected type, advance to the token.
+		p.readNextToken()
+		return true
+	}
+	return false
+}
+
+func (p *Parser) storeNextTokenTypeError(expectType token.LexicalType) {
+	errMsg := fmt.Sprintf("error next token type: expected TYPE(%s), got TYPE(%s).", expectType, p.nextToken.Type)
+	p.errors = append(p.errors, errMsg)
+}
+
+//func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+//	return nil
+//}
+//
+//func (p *Parser) parseExpression() ast.Expression {
+//	return nil
+//}
+//
